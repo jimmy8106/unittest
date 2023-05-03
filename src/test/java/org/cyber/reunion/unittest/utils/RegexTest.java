c7c92c85757b7d2388e0e68e4a33d9439db65428
jimmy8106 <loktar8106@gmail.com>
2023-05-03 17:34:28 +0800

    FIX UTILS

--- src/test/java/org/cyber/reunion/unittest/utils/RegexTest.java
+++ src/test/java/org/cyber/reunion/unittest/utils/RegexTest.java
@@ -2,22 +2,19 @@ package org.cyber.reunion.unittest.utils;
 
 import org.cyber.reunion.unittest.common.RegexUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
 public class RegexTest {
-  @Autowired
-  private RegexUtils utils;
   
   @Test
   public void validateValidEmail() {
     String email = "abc@gmail.com";
-    Assert.isTrue(utils.patternMatches(email, utils.emailRegexPattern), "must be true");
+    Assert.isTrue(RegexUtils.patternMatches(email, RegexUtils.emailRegexPattern), "must be true");
   }
   
   @Test
   public void validateInvalidEmail() {
     String email = "@gmail.com";
-    Assert.isTrue(false == utils.patternMatches(email, utils.emailRegexPattern), "must be false");
+    Assert.isTrue(false == RegexUtils.patternMatches(email, RegexUtils.emailRegexPattern), "must be false");
   }
 }
